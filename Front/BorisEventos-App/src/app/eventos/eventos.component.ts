a579e84357c354f3803402a0442ce0bb862c151f
caio.correa <caio.correa@jjconsulting.com.br>
2022-04-26 01:20:38 -0300

    Eventos Grid development

--- Front/BorisEventos-App/src/app/eventos/eventos.component.ts
+++ Front/BorisEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+public eventosFiltrados: any = [];
+
+  widthImg = 200;
+  marginImg = 2;
+  showImage = false;
+  private _filterList = '';
+
+  public get filterList(){
+    return this._filterList;
+  }
+
+  public set filter(value : string){
+    this._filterList = value;
+    this.eventosFiltrados = this._filterList ? this.filterEventos(this._filterList) : this.eventos;
+  }
+
+  filterEventos(filterBy: string): any{
+      filterBy = filterBy.toLowerCase();
+      return this.eventos.filter(
+        (evento : { tema: string; local:string}) => evento.tema.toLowerCase().indexOf(filterBy) !== -1 ||
+         evento.local.toLowerCase().indexOf(filterBy) !== -1 )
+  }
+
+  collapseImage (){
+    this.showImage = !this.showImage;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +45,10 @@ export class EventosComponent implements OnInit {
   public getEventos():void{
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
 }
